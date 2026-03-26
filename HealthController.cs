1dad66e7e10b06c84039b281fdde18860ae1db9e
OrdinaryRock1 <trunghoanghai0@gmail.com>
2026-03-26 20:48:56 +0100

    Add files via upload

---
+++ HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Recepty;
+
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    [HttpHead]
+    public IActionResult Get()
+    {
+        return Ok(new { status = "ok" });
+    }
+}
